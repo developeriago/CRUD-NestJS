3a57e1cf65d5340ab6b840fd78a2d2fbcd0b05f6
Iago Almeida <iagoalmeidadev@gmail.com>
2023-05-01 23:42:01 -0300

    CRUD Finalizado!

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,16 @@
+/* eslint-disable prettier/prettier */
+import { IsEmail, IsString, MinLength } from "class-validator";
+
+export class CreateUserDto {
+
+    @IsEmail()
+    email: string;
+
+    @IsString()
+    name: string;
+
+    @IsString()
+    @MinLength(6)
+    password: string;
+
+}
